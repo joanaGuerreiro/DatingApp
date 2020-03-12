d25f5e776fd6a5835aafd80ae079e58f2fbdca6a
joanaGuerreiro <joana.guerreiro@reditpro.com>
2020-03-12 10:06:18 +0000

    End of section 4

--- DatingApp2/Controllers/WeatherForecastController.cs
+++ DatingApp2/Controllers/WeatherForecastController.cs
@@ -20,6 +20,7 @@ namespace DatingApp2.Controllers
         {
             _context = context;
         }
+        [AllowAnonymous]
         [HttpGet]
         public  async Task<IActionResult> GetValues()
         {
