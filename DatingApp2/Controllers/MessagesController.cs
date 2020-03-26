44e1a68924822c9a15f134c428a80488c95d6793
joanaGuerreiro <joana.guerreiro@reditpro.com>
2020-03-26 12:35:03 +0000

    End of Section 16

---
+++ DatingApp2/Controllers/MessagesController.cs
@@ -0,0 +1,173 @@
+﻿using AutoMapper;
+using DatingApp2.Data;
+using DatingApp2.Dtos;
+using DatingApp2.Helpers;
+using DatingApp2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DatingApp2.Controllers
+{
+    [ServiceFilter(typeof(LogUsersActivity))]
+    [Authorize]
+    [ApiController]
+    [Route("/users/{userId}/[controller]")]
+    public class MessagesController:ControllerBase
+    {
+        private IDatingRepository _repo;
+        private IMapper _mapper;
+
+        public MessagesController(IDatingRepository repo,IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+        
+        [HttpGet("{id}",Name ="GetMessage")]
+
+        public async Task<IActionResult> GetMessage(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if(messageFromRepo==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(messageFromRepo);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesForUser(int userId, [FromQuery]MessageParams messageParams)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            messageParams.UserId = userId;
+            var messageFromRepo = await _repo.GetMessagesForUser(messageParams);
+            
+            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messageFromRepo);
+
+            Response.AddPagination(messageFromRepo.CurrentPage, messageFromRepo.PageSize
+                , messageFromRepo.TotalCount, messageFromRepo.TotalPages);
+
+            return Ok(messages);
+        }
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult>GetMessageThread(int userId,int recipientId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var messageFromRepo = await _repo.GetMessageThread(userId, recipientId);
+            var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messageFromRepo);
+
+            return Ok(messageThread);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>CreatedMessage(int userId,MessageForCreationDto messageForCreationDto)
+        {
+
+            var sender = await _repo.GetUser(userId);
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            messageForCreationDto.SenderId = userId;
+            var recipient = await _repo.GetUser(messageForCreationDto.RecipientId);
+
+            if(recipient == null)
+            {
+                return BadRequest("Could not find user");
+            }
+
+            var message = _mapper.Map<Message>(messageForCreationDto);
+            _repo.Add(message);
+
+          
+
+            if(await _repo.SaveAll())
+            {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(message);
+                var route = CreatedAtAction(nameof(MessagesController.GetMessage), new { controller = "Messages", id = message.Id }, messageToReturn);
+                return StatusCode(201, messageToReturn);
+            }
+
+            throw new Exception("Creating the message failed on save");
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult>DeleteMessage(int id, int userId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if(messageFromRepo.SenderId == userId)
+            {
+                messageFromRepo.SenderDeleted = true;
+            }
+
+            if (messageFromRepo.RecipientId == userId)
+            {
+                messageFromRepo.RecipientDeleted = true;
+            }
+
+            if(messageFromRepo.SenderDeleted && messageFromRepo.RecipientDeleted)
+            {
+                _repo.Delete(messageFromRepo);
+            }
+
+            if(await _repo.SaveAll())
+            {
+                return NoContent();
+            }
+
+            throw new Exception("Error deleting the message");
+        }
+
+         [HttpPost("{id}/read")]
+
+         public async Task<IActionResult> MarkMessageAsRead(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var message = await _repo.GetMessage(id);
+
+            if(message.RecipientId != userId)
+            {
+                return Unauthorized();
+            }
+
+            message.IsRead = true;
+            message.DaterRead = DateTime.Now;
+
+            await _repo.SaveAll();
+
+            return NoContent();
+
+        }
+    }
+}
---
+++ DatingApp2/Dtos/MessageToReturnDto.cs
@@ -0,0 +1,23 @@
+﻿using DatingApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp2.Dtos
+{
+    public class MessageToReturnDto
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public string SenderKownAs { get; set; }
+        public string SenderPhotoUrl { get; set; }
+        public int RecipientId { get; set; }
+        public string RecipientKownAs { get; set; }
+        public string RecipientPhotoUrl { get; set; }
+        public string Content { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DaterRead { get; set; }
+        public DateTime MessageSent { get; set; }
+    }
+}
